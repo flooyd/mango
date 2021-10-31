3da114b1f966d74df1bab7c591b8253f3265156d
Floyd Jones <floydtjones@gmail.com>
2021-10-31 11:40:26 -0600

    remove auto exec of parser...not sure how to do

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as child from 'child_process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,12 +7,8 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
   });
-  /* const odotaparser = child.spawn('java', [
-    '-jar',
-    '../odotaparser/stats-0.1.0.jar',
-  ]); */
+
   await app.listen(8080);
-  // process.on('exit', () => odotaparser.kill());
 }
 
 bootstrap();
